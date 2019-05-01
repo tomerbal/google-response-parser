c5e688c687789ab7c4c3ee628af33885e7c0056f
tomer <tomerbalan@gmail.com>
2019-05-02 00:27:30 +0300

    Added pla

--- index.js
+++ index.js
@@ -23,7 +23,21 @@ function parse(response, inputUrl) {
     if (output.objects[0].results) {
         console.log("number of results: " + output.objects[0].results.length);
     }
+    setQuery(output.objects[0], inputUrl);
     return output;
+}
+
+function setQuery(output, url){
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+    output.query = decodeURIComponent(getParameterByName("q", url));
 }
 
 function setDictionary($, output) {
